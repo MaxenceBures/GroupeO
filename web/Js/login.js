ad6c96c00fd6cf44cda18f96016a547ad01a23d7
MaxenceBures <maxence.bures@gmail.com>
2017-06-18 18:47:12 +0200

    Connexion + Liste Utilisateurs

---
+++ web/Js/login.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Maxence on 18/06/2017.
+ */
+$(function() {
+    $('#modal_password').hide();
+    $('#open_modal').click(function() {
+        $('#modal_password').show();
+
+    })
+    $('#close_modal').click(function() {
+        $('#modal_password').hide();
+
+    })
+});
---
+++ web/Js/utilisateur/liste.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Maxence on 18/06/2017.
+ */
+$(function() {
+
+    $('#liste_utilisateur').DataTable( {
+        "paging":   true,
+        "ordering": false,
+        "info":     false
+    } );
+});
