3643b7569695118b6202fc8f37361ae15c0d1fb5
MaxenceBures <maxence.bures@gmail.com>
2017-06-20 15:59:55 +0200

    Liste Utilisateur
    Add + Update + Remove OK
    New password KO
    Design KO

--- src/FrontendBundle/Controller/UtilisateurController.php
+++ src/FrontendBundle/Controller/UtilisateurController.php
@@ -38,7 +38,7 @@ class UtilisateurController extends Controller
             $utilisateur_temp = $request->get("utilisateur");
 
             $utilisateur = new Utilisateur();
-            $utilisateur->setName($utilisateur_temp["nom"]." ".$utilisateur_temp["prenom"]);
+            $utilisateur->setName($utilisateur_temp["nom"]);
             $utilisateur->setEmail($utilisateur_temp["mail"]);
             $utilisateur->setPassword(sha1($utilisateur_temp["motdepasse"]));
             $utilisateur->setIsActive(TRUE);
@@ -57,7 +57,88 @@ class UtilisateurController extends Controller
             /*$repository = $this->getDoctrine()->getRepository('FrontendBundle:Utilisateur');
             $repository->insertUtilisateur($utilisateur)*/
 
-            return new Response(json_encode(array("status" =>"ok","utilisateur_id" => $utilisateur->getId())));
+            return new Response(json_encode(array("status" =>"ok","utilisateur_id" => $utilisateur->getId(), "utilisateur_role" => $utilisateur->getRole())));
+        }
+
+        return "error";
+    }
+
+    /**
+     * @Route("/utilisateur/modifier", name="utilisateur_modifier")
+     */
+    public function modifierAction(Request $request){
+
+        if ($request->isXMLHttpRequest()) {
+
+            $em = $this->getDoctrine()->getManager();
+            $repository = $em->getRepository('FrontendBundle:Utilisateur');
+            $utilisateur_temp = $request->get("utilisateur");
+
+            $utilisateur = $repository->find($utilisateur_temp["id"]);
+            $utilisateur->setName($utilisateur_temp["nom"]);
+            $utilisateur->setEmail($utilisateur_temp["mail"]);
+
+            if($utilisateur_temp["role"] == "Administrateur"){
+                $utilisateur->setRole("ROLE_ADMIN");
+            }else{
+                $utilisateur->setRole("ROLE_USER");
+            }
+
+            $em->persist($utilisateur);
+            $em->flush();
+
+            return new Response(json_encode(array("status" =>"ok", "utilisateur_role" => $utilisateur->getRole())));
+        }
+
+        return "error";
+    }
+
+    /**
+     * @Route("/utilisateur/supprimer", name="utilisateur_supprimer")
+     */
+    public function supprimerAction(Request $request){
+
+        if ($request->isXMLHttpRequest()) {
+
+            $em = $this->getDoctrine()->getManager();
+            $repository = $em->getRepository('FrontendBundle:Utilisateur');
+            $utilisateur_temp = $request->get("utilisateur");
+
+            $utilisateur = $repository->find($utilisateur_temp["id"]);
+
+
+            $em->remove($utilisateur);
+            $em->flush();
+
+            return new Response(json_encode(array("status" =>"ok")));
+        }
+
+        return "error";
+    }
+
+
+    /**
+     * @Route("/utilisateur/envoi_mdp", name="utilisateur_envoi_mdp")
+     */
+    public function envoi_mdpAction(Request $request){
+
+        if ($request->isXMLHttpRequest()) {
+
+            $em = $this->getDoctrine()->getManager();
+            $repository = $em->getRepository('FrontendBundle:Utilisateur');
+            $utilisateur_temp = $request->get("utilisateur");
+
+            $utilisateur = $repository->find($utilisateur_temp["id"]);
+
+            $pass = trim($utilisateur->getName());
+
+            $utilisateur->setPassword(sha1($pass));
+
+
+            $em->persist($utilisateur);
+            $em->flush();
+
+            return new Response(json_encode(array("status" =>"ok")));
         }
 
         return "error";
