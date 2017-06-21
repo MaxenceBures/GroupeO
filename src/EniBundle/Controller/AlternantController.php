45e30583ef1730c85a7baae037fa2e000621503b
MaxenceBures <maxence.bures@gmail.com>
2017-06-21 17:18:52 +0200

    Test

--- src/EniBundle/Controller/AlternantController.php
+++ src/EniBundle/Controller/AlternantController.php
@@ -7,6 +7,8 @@ use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Common\Persistence\ManagerRegistry;
 use Doctrine\DBAL\DriverManager;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class AlternantController extends Controller
 {
@@ -20,5 +22,35 @@ class AlternantController extends Controller
         }
 
         return $this->render('EniBundle:Alternant:recherche.html.twig');
+    }
+
+    /**
+     * @Route("/alternant/chargement", name="alternant_chargement")
+     */
+    public function chargementAction(Request $request)
+    {
+        if ($request->isXMLHttpRequest()) {
+
+            $recherche_temp = $request->get("recherche");
+
+            dump($recherche_temp);
+            die("");
+
+            /*
+            $em = $this->getDoctrine()->getManager('eni');
+            $repository = $em->getRepository('EniBundle:Alternant');
+
+
+            $formations_temp = $repository->findAll();
+
+            $temp = array();
+            foreach ($formations_temp as $x){
+                array_push($temp,array('court' => $x->getLibellecourt(), 'long' => $x->getLibellelong(), 'id' => $x->getCodeformation()));
+            }*/
+
+
+            return new Response(json_encode(array("status" =>"ok")));
+        }
+        return new Response(json_encode("error"));
     }
 }
--- src/EniBundle/Controller/FormationController.php
+++ src/EniBundle/Controller/FormationController.php
@@ -3,7 +3,36 @@
 namespace EniBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\DBAL\DriverManager;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class FormationController extends Controller
 {
+    /**
+     * @Route("/formation/liste", name="formation_liste")
+     */
+    public function listeAction(Request $request)
+    {
+        if ($request->isXMLHttpRequest()) {
+
+            $em = $this->getDoctrine()->getManager('eni');
+            $repository = $em->getRepository('EniBundle:Formation');
+
+
+            $formations_temp = $repository->findAll();
+
+            $temp = array();
+            foreach ($formations_temp as $x){
+                array_push($temp,array('court' => $x->getLibellecourt(), 'long' => $x->getLibellelong(), 'id' => $x->getCodeformation()));
+            }
+
+
+            return new Response(json_encode(array("status" =>"ok","formations" => json_encode($temp))));
+        }
+        return new Response(json_encode("error"));
+    }
 }
